de123104a96fd0924e2dde121e173ac28462dc55
donal <donalspring@gmail.com>
2018-03-31 23:10:42 +0100

    remove html and add package stuff

--- server/routes.js
+++ server/routes.js
@@ -19,6 +19,7 @@ module.exports = function(app) {
   // All other routes should redirect to the index.html
   app.route('/*')
     .get(function(req, res) {
-      res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
+      return res.status(200).json({OK: true, timestamp: Date.now()});
+      // res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
     });
 };
