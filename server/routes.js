2bb44eb0c09150f47137955f6a0d2ae55b714fa4
donal <donalspring@gmail.com>
2018-04-04 16:12:24 +0100

    ADDING delete functionality

--- server/routes.js
+++ server/routes.js
@@ -12,6 +12,7 @@ module.exports = function(app) {
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     next();
   });
   // Insert routes below
